e33333f075835971d8535c06c7c5fe2032555fce
WouterJ <waldio.webdesign@gmail.com>
2014-05-21 19:08:01 +0200

    Tweaked build script

--- build_demo.sh
+++ build_demo.sh
@@ -1,11 +1,19 @@
 git config --global user.name "Sf Travis Bot"
+git config --global user.email "sf-travis-bot@travis-ci.org"
 
 git clone https://${GH_TOKEN}@github.com/WouterJ/symfony-cmf-docs-demo
+
+if [[ ${TRAVIS_PULL_REQUEST} ]]
+    DEMO_DIR_NAME=symfony-cmf-docs-demo/${TRAVIS_PULL_REQUEST}-${TRAVIS_BUILD_NUMBER}
+else
+    DEMO_DIR_NAME=symfony-cmf-docs-demo/${TRAVIS_REPO_SLUG//\//-}-${TRAVIS_BRANCH}-${TRAVIS_BUILD_NUMBER}
+fi
 
 DEMO_DIR_NAME=symfony-cmf-docs-demo/${TRAVIS_PULL_REQUEST}-${TRAVIS_BUILD_NUMBER}
 
 mkdir $DEMO_DIR_NAME
-cp -rf _build/html $DEMO_DIR_NAME
+
+cp -rf _build/html/* $DEMO_DIR_NAME
 
 cd symfony-cmf-docs-demo
 
