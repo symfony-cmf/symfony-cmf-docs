de1e63d191dea20a5ba6a4b0508fa80f43a9d394
WouterJ <waldio.webdesign@gmail.com>
2014-05-22 10:34:07 +0200

    Tried to fix dir naming

--- build_demo.sh
+++ build_demo.sh
@@ -1,29 +1,34 @@
+# give me a nice name
 git config --global user.name "Sf Travis Bot"
 git config --global user.email "sf-travis-bot@travis-ci.org"
 
+# clone the demo repository
 git clone https://${GH_TOKEN}@github.com/WouterJ/symfony-cmf-docs-demo
 
-if [[ ${TRAVIS_PULL_REQUEST} ]]; then
-    DEMO_PREFIX=${TRAVIS_PULL_REQUEST}
-else
+# create a new directory name
+if [[ "false" == ${TRAVIS_PULL_REQUEST} ]]; then
     DEMO_PREFIX=${TRAVIS_BRANCH}
+else
+    DEMO_PREFIX=${TRAVIS_PULL_REQUEST}
 fi
 
 DEMO_DIR_NAME=symfony-cmf-docs-demo/${DEMO_PREFIX}-${TRAVIS_BUILD_NUMBER}
 
+# remove all previous demos for this PR
 rm -rf symfony-cmf-docs-demo/${DEMO_PREFIX}-*
 
 mkdir $DEMO_DIR_NAME
 
+# copy the build to the demo dir
 cp -rf _build/html/* $DEMO_DIR_NAME
 
 cd symfony-cmf-docs-demo
 
-touch .nojekyll
-
+# commit all removed and added files
 git add -A
-
 git commit -m "Travis build ${TRAVIS_BUILD_NUMBER}"
+
+# push to origin
 git push origin
 
 if [ $? != 0 ]; then
